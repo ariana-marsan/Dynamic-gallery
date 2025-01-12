738ba403a96acd6f11cc8075ebfff0e6b5888ee5
ariana <arimarsan6@gmail.com>
2025-01-12 12:10:55 -0300

    Add files via upload

--- code.js
+++ code.js
@@ -14,3 +14,20 @@ uploader.addEventListener("dragleave",e => {
     e.preventDefault();
     changeStyle(e.srcElement, "#4447");
 })
+
+const uploadFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener("load",e => {
+        let url = URL.createObjectURL(file);
+        let img = document.createElement("IMG");
+        img.setAttribute("src", url);
+        document.querySelector(".photo_container").appendChild(img);
+    })
+}
+
+uploader.addEventListener("drop",e => {
+    e.preventDefault();
+    changeStyle(e.srcElement, "#4447");
+    uploadFile(e.dataTransfer.files[0]);
+})
